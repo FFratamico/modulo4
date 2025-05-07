04fb51efa10cff79947db156073a4d53220e1f01
FFratamico <faustofratamico1@gmail.com>
2025-05-07 14:37:50 -0300

    Actualizacion

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -3,17 +3,19 @@ import {config} from 'dotenv';
 config({path: '.env.development'});
 import { DataSource, DataSourceOptions } from "typeorm";
 
+const isDocker = process.env.NODE_ENV === 'docker';
+
 export const AppDataSource: DataSourceOptions = ({
     type: 'postgres',
-    host: process.env.DB_HOST_DOCKER,
-    port: parseInt(process.env.DB_PORT_DOCKER!, 10),
+    host: isDocker ? process.env.DB_HOST_DOCKER : process.env.DB_HOST,
+    port: parseInt(isDocker ? process.env.DB_PORT_DOCKER : process.env.DB_PORT!, 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: false,
+    synchronize: process.env.NODE_ENV === 'docker' ? false : false,
     entities: ['dist/**/*.entity{.js,.ts}'], // busca automaticamente todas las entidades del proyecto.
     migrations: ['dist/migrations/*{.ts,.js}'],
-    ssl: { rejectUnauthorized: false }
+    ssl: isDocker ? { rejectUnauthorized: false } : false,
 });
 
 export const postgresDataSourceConfig = registerAs(
